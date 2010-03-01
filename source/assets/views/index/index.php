a6210117cee9c0286849ee9048d2137162e8aa55
Amanda Steigerwalt <asteig@gmail.com>
2010-02-28 23:29:45 -0500

    getting rid of views folder. slid in wrong place

--- source/assets/views/index/index.php
+++
@@ -1,5 +0,0 @@
-<h2>Welcome to RabidCore!</h2>
-
-<p>The template file resides in views/template.php, and this text is in views/index/index.php.</p>
-
-<p>You'll probably want to modify these files to do something more useful.</p>
